bf7670f2424f1d3aa6b16799ef9ad87cfa5e1a7a
pendragon707 <pendragon707@yandex.ru>
2024-11-18 15:19:13 +0300

    fix run2 bah

--- run2.bash
+++ run2.bash
@@ -1,3 +1,3 @@
 source install/setup.sh
 source /opt/ros/humble/setup.bash
-ros2 run cv_basics img_subscriber
+ros2 run image_transport republish  compressed raw --ros-args -r in/compressed:=/image_raw/compressed -r out:=/image_raw/uncomp
